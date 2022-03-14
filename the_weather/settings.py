a0a78569a0dbaf8facacb230a9adf64132137d3f
Wiwin Mafiroh <wiwinmafiroh@gmail.com>
2022-03-14 22:09:03 +0700

    Added allowed hosts

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$81j!fy9)qt3!m+u5va0ten437yq-svqn%4-&x-_bz*we*sh&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wiwinmafiroh-django-weather.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
